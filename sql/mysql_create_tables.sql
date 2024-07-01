f659eb7b7a14cbcc323a9cc3fdf9ff2ed3d1d059
SOME Fiarma Landry <SOME Fiarma Landry@fiarmasome@gmail.com>
2024-07-01 22:35:01 +0000

    Commit message

---
+++ sql/mysql_create_tables.sql
@@ -0,0 +1,14 @@
+-- Active: 1719799072939@@127.0.0.1@3306@amdaridb
+
+CREATE TABLE IF NOT EXISTS university(
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    founded INT NOT NULL,
+    `type` VARCHAR(255) NOT NULL,
+    enrollment  BIGINT NOT NULL,
+    link VARCHAR(255) NOT NULL,
+
+    UNIQUE KEY unique_combination(`country`, `name`),
+    PRIMARY KEY(id)
+);
